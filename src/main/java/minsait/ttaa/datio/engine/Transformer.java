bcfd5a8e70019808bc3ca4f01cd19c8ad1415676
chynobarza <tolucachido_7@hotmail.com>
2021-07-18 00:20:33 -0500

    Update Transformer.java

--- src/main/java/minsait/ttaa/datio/engine/Transformer.java
+++ src/main/java/minsait/ttaa/datio/engine/Transformer.java
@@ -1,5 +1,6 @@
 package minsait.ttaa.datio.engine;
 
+import minsait.ttaa.datio.utils.Properties;
 import org.apache.spark.sql.Column;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
@@ -11,47 +12,67 @@ import org.jetbrains.annotations.NotNull;
 import static minsait.ttaa.datio.common.Common.*;
 import static minsait.ttaa.datio.common.naming.PlayerInput.*;
 import static minsait.ttaa.datio.common.naming.PlayerOutput.*;
-import static org.apache.spark.sql.functions.*;
+import static org.apache.spark.sql.functions.rank;
+import static org.apache.spark.sql.functions.when;
 
 public class Transformer extends Writer {
     private SparkSession spark;
 
     public Transformer(@NotNull SparkSession spark) {
+
+        Properties properties = new Properties();
+
+
+        System.setProperty("hadoop.home.dir", "C:\\hadoop");
         this.spark = spark;
-        Dataset<Row> df = readInput();
+        Dataset<Row> df = readInput(properties.position(0));
 
         df.printSchema();
 
         df = cleanData(df);
+        if (properties.position(2).equals(number_1)) {
+            df = menores23(df);
+        }
         df = exampleWindowFunction(df);
+        df = exampleWindowFunction2(df);
+        df = exampleWindowFunction3(df);
+        df = exampleWindowFunction4(df);
         df = columnSelection(df);
 
         // for show 100 records after your transformations and show the Dataset schema
         df.show(100, false);
-        df.printSchema();
 
         // Uncomment when you want write your final output
-        //write(df);
+        write(df, properties.position(1));
+        //write2(df);
     }
 
     private Dataset<Row> columnSelection(Dataset<Row> df) {
         return df.select(
                 shortName.column(),
-                overall.column(),
+                longName.column(),
+                age.column(),
                 heightCm.column(),
+                weightKg.column(),
+                nationality.column(),
+                clubName.column(),
+                overall.column(),
+                potential.column(),
                 teamPosition.column(),
-                catHeightByPosition.column()
+                catHeightByPosition.column(),
+                playerCat.column(),
+                potentialVsOverall.column()
         );
     }
 
     /**
      * @return a Dataset readed from csv file
      */
-    private Dataset<Row> readInput() {
+    private Dataset<Row> readInput(String file) {
         Dataset<Row> df = spark.read()
                 .option(HEADER, true)
                 .option(INFER_SCHEMA, true)
-                .csv(INPUT_PATH);
+                .csv(file);
         return df;
     }
 
@@ -87,16 +108,63 @@ public class Transformer extends Writer {
 
         Column rank = rank().over(w);
 
-        Column rule = when(rank.$less(10), "A")
-                .when(rank.$less(50), "B")
-                .otherwise("C");
+        Column rule = when(rank.$less(20), A)
+                .when(rank.$less(50), B)
+                .otherwise(C);
 
         df = df.withColumn(catHeightByPosition.getName(), rule);
 
         return df;
     }
 
+    /**
+     * @param df is a Dataset with players information (must have team_position and height_cm columns)
+     * @return add to the Dataset the column "cat_height_by_position"
+     * by each position value
+     * cat A for if is in 20 players tallest
+     * cat B for if is in 50 players tallest
+     * cat C for the rest
+     */
+    private Dataset<Row> exampleWindowFunction2(Dataset<Row> df) {
+        WindowSpec w = Window
+                .partitionBy(nationality.column(), teamPosition.column())
+                .orderBy(overall.column().desc());
 
+        Column rank = rank().over(w);
+
+        Column rule = when(rank.$less(3), A)
+                .when(rank.$less(5), B)
+                .when(rank.$less(10), C)
+                .otherwise(D);
+
+        df = df.withColumn(playerCat.getName(), rule);
+
+        return df;
+    }
+
+    private Dataset<Row> exampleWindowFunction3(Dataset<Row> df) {
+
+        df = df.withColumn(potentialVsOverall.getName(), potential.column().divide(overall.column()));
+
+        return df;
+    }
+
+    private Dataset<Row> exampleWindowFunction4(Dataset<Row> df) {
+
+        df = df.filter(playerCat.column().equalTo(A)
+                .or(playerCat.column().equalTo(B))
+                .or(playerCat.column().equalTo(C).and(potentialVsOverall.column().geq(1.15)))
+                .or(playerCat.column().equalTo(D).and(potentialVsOverall.column().geq(1.25))));
+
+        return df;
+    }
+
+    private Dataset<Row> menores23(Dataset<Row> df) {
+
+        df = df.filter(age.column().lt(23));
+
+        return df;
+    }
 
 
 }
