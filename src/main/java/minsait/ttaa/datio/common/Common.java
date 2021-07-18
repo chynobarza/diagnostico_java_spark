3e27c0ce4f080cbc8acce2708e6318d597f30495
chynobarza <tolucachido_7@hotmail.com>
2021-07-18 00:22:35 -0500

    Update Common.java

--- src/main/java/minsait/ttaa/datio/common/Common.java
+++ src/main/java/minsait/ttaa/datio/common/Common.java
@@ -5,7 +5,12 @@ public final class Common {
     public static final String SPARK_MODE = "local[*]";
     public static final String HEADER = "header";
     public static final String INFER_SCHEMA = "inferSchema";
-    public static final String INPUT_PATH = "src/test/resources/data/players_21.csv";
-    public static final String OUTPUT_PATH = "src/test/resources/data/output";
+    public static final String PARAMS_PATH = "src/test/resources/params";
+
+    public static final String number_1 = "1";
+    public static final String A = "A";
+    public static final String B = "B";
+    public static final String C = "C";
+    public static final String D = "D";
 
 }
